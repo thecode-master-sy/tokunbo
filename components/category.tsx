65c191578038070b769b0144ff77f782101d77b3
thecode-master-sy <thecodemasster@gmail.com>
2026-04-19 06:00:55 +0100

    feat: increase font size for category

--- components/category.tsx
+++ components/category.tsx
@@ -4,7 +4,7 @@ import { Category as CategoryType } from "@/lib/product-types";
 
 export default function Category({ category }: { category: CategoryType }) {
   return (
-    <div className="font-mono flex gap-2 uppercase text-[11px] md:text-caps bg-category md:py-2 md:px-4 px-3 py-1  items-center rounded-full">
+    <div className="font-mono flex cursor-pointer gap-2 uppercase text-[12px] md:text-caps bg-category md:py-2 md:px-4 px-3 py-1  items-center rounded-full">
       <div className="w-6 h-6 md:w-10 md:h-10 flex justify-center items-center rounded-full bg-banner p-1">
         <Image
           src={category.imageUrl}
