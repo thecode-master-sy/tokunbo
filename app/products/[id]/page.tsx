b36126069454fc323902e9b9ceb58e6984884a69
thecode-master-sy <thecodemasster@gmail.com>
2026-04-21 12:43:10 +0100

    feat: fix recommended products type error

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,11 +1,11 @@
 import ProductDetails from "@/app/products/_sections/product-details";
-import RecommendedProducts from "@/app/products/_sections/recommended-products";
+//import RecommendedProducts from "@/app/products/_sections/recommended-products";
 
 export default function Product() {
   return (
     <div className="px-4 lg:px-6">
       <ProductDetails />
-      <RecommendedProducts />
+      {/*<RecommendedProducts />*/}
     </div>
   );
 }
