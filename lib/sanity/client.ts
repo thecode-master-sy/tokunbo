1af9eb747ed1027a9888e02645f98dc676887fee
thecode-master-sy <thecodemasster@gmail.com>
2026-04-21 15:39:02 +0100

    feat: fix project id must be specified

--- lib/sanity/client.ts
+++ lib/sanity/client.ts
@@ -1,9 +1,9 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+  projectId: "e4e3wdqx",
+  dataset: "production",
+  apiVersion: "2025-04-20",
   useCdn: false,
   token: process.env.SANITY_API_READ_TOKEN!,
 });
