84dc66d585cee4f291c92791bfa847ff99b661e8
thecode-master-sy <thecodemasster@gmail.com>
2026-05-08 08:23:39 +0100

    feat: checkout flow

--- env.ts
+++ env.ts
@@ -5,12 +5,11 @@ import { ZodError, z } from "zod";
 const EnvSchema = z.object({
   NODE_ENV: z.string().default("development"),
   DATABASE_URL: z.string(),
-  //RESEND_API_KEY: z.string(),
+  // RESEND_API_KEY: z.string(),
   UPSTASH_REDIS_REST_URL: z.string(),
   UPSTASH_REDIS_REST_TOKEN: z.string(),
   PAYSTACK_SECRET_KEY: z.string(),
-  PAYSTACK_PUBLIC_KEY: z.string()
-  
+  PAYSTACK_PUBLIC_KEY: z.string(),
 });
 
 export type EnvSchema = z.infer<typeof EnvSchema>;
@@ -22,9 +21,11 @@ try {
 } catch (error) {
   if (error instanceof ZodError) {
     let message = "Missing required values in .env:\n";
+
     error.issues.forEach((issue) => {
-      message += issue.path[0] + "\n";
+      message += String(issue.path[0]) + "\n";
     });
+
     const e = new Error(message);
     e.stack = "";
     throw e;
@@ -33,4 +34,6 @@ try {
   }
 }
 
-export default EnvSchema.parse(process.env);
+const env = EnvSchema.parse(process.env);
+
+export default env;
