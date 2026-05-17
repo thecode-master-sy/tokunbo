93a080b14992284ab2ecfd65624af5fcef43166d
thecode-master-sy <thecodemasster@gmail.com>
2026-05-17 16:45:08 +0100

    feat: cart now uses variant id to add item to cart

--- components/cart-item-component.tsx
+++ components/cart-item-component.tsx
@@ -43,7 +43,7 @@ export default function CartItemComponent({
         <div className="flex gap-2">
           <div className="flex w-full items-center bg-banner rounded-sm gap-1">
             <button
-              className="p-2 flex-1"
+              className="p-2 flex-1 flex items-center justify-center"
               onClick={() => decreaseQty(id)}
               disabled={quantity <= 1}
             >
@@ -52,7 +52,7 @@ export default function CartItemComponent({
 
             <span className="w-4 text-center">{quantity}</span>
 
-            <button className="p-2 flex-1" onClick={() => increaseQty(id)}>
+            <button className="p-2 flex-1 flex items-center justify-center" onClick={() => increaseQty(id)}>
               <Plus className="w-3 h-3" />
             </button>
           </div>
